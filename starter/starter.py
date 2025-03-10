d5a087b8b16176b9c58be1e179575f63bc840740
AnupJoseph <anup20joseph@gmail.com>
2025-03-10 13:11:23 +0000

    Small changes

--- starter/starter.py
+++ starter/starter.py
@@ -1,4 +1,22 @@
 import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
 
 
-base_model = tf.keras.applications.MobileNetV2(input_shape = [128,128,3],include_top=False)
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=[128, 128, 3], include_top=False
+)
+
+layer_names = [
+    "block_1_expand_relu",
+    "block_3_expand_relu",
+    "block_6_expand_relu",
+    "block_13_expand_relu",
+    "block_16_project",
+]
+
+layers = [base_model.get_layer(n) for n in layer_names]
+
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+down_stack.trainable = False
+
+up_stack = [p]
