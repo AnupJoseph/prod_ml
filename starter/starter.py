949c678c6352c558c94c90eb8a5950a849c12af7
AnupJoseph <anup20joseph@gmail.com>
2025-03-12 04:28:38 +0000

    Finished starter code

--- starter/starter.py
+++ starter/starter.py
@@ -19,4 +19,28 @@ layers = [base_model.get_layer(n) for n in layer_names]
 down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
 down_stack.trainable = False
 
-up_stack = [p]
+up_stack = [
+    pix2pix.upsample(512, 3),
+    pix2pix.upsample(256, 3),
+    pix2pix.upsample(128, 3),
+    pix2pix.upsample(64, 3),
+]
+
+def unet_model(output_channels):
+    inputs = tf.keras.layers.Input(shape=[128,128,3])
+    x = inputs
+
+    skips = down_stack(x)
+    x = skips[-1]
+    skips = reversed(skips[:-1])
+
+    for up,skip in zip(up_stack,skips):
+        x = up(x)
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x,skip])
+
+    last = tf.keras.layers.Conv2DTranspose(output_channels,3,strides=2,padding="same")
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs,outputs=x)
+
